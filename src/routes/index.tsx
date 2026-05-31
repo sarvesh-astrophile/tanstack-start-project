5de3a10cee0199e1570ce834b338689f35b74353
sarvesh-astrophile <sarvesh9301@gmail.com>
2026-05-31 15:59:20 +0530

    Move navbar into route components and configure Biome

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,16 @@
-import { Navbar } from "#/components/web/navbar";
 import { createFileRoute } from "@tanstack/react-router";
+import { Navbar } from "#/routes/-components/navbar";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-    return (
-        <div className="">
-            <Navbar />
-            <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-            <p className="mt-4 text-lg">
-                Edit <code>src/routes/index.tsx</code> to get started.
-            </p>
-        </div>
-    );
+	return (
+		<div className="">
+			<Navbar />
+			<h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+			<p className="mt-4 text-lg">
+				Edit <code>src/routes/index.tsx</code> to get started.
+			</p>
+		</div>
+	);
 }
